802790e04ff209cff11d25d6ae6f11752291123f
Julien Hamaide <julien.hamaide@gmail.com>
2024-08-14 00:37:09 +0200

    Top package : NVN renderer fixes + Orbis multistream support

--- conan/conanfile.py
+++ conan/conanfile.py
@@ -7,7 +7,7 @@ from conan.tools.files import load, update_conandata
 
 class BgfxConan(ConanFile):
     name            = "bgfx"
-    version         = "7816-22"
+    version         = "7816-23"
     description     = "Conan package for bgfx."
     url             = "https://github.com/bkaradzic/bgfx"
     license         = "BSD"
